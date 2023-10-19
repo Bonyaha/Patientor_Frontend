93b9ff881381d1a77ec937624647c5216693bec7
Roman <romchikus1987@gmail.com>
2023-10-19 17:04:32 +0300

    updated

--- src/components/PatientListPage/PatientDetailPage.tsx
+++ src/components/PatientListPage/PatientDetailPage.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Typography, List, ListItem, ListItemText } from '@mui/material';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
 import patientService from '../../services/patients';
-import { Patient, Gender } from "../../types";
+import diagnosesService from '../../services/diagnoses';
+
+import { Diagnosis, Patient, Gender } from "../../types";
 import TransgenderIcon from '@mui/icons-material/Transgender';
 
 
@@ -12,6 +14,7 @@ const PatientDetailPage = () => {
 	const params = useParams();
 	const id = params.id as string; // Extract and cast id to string
 	const [patient, setPatient] = useState<Patient | null>(null);
+	const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
 	useEffect(() => {
 		const fetchPatientData = async () => {
@@ -22,9 +25,19 @@ const PatientDetailPage = () => {
 				console.error('Error fetching patient data:', error);
 			}
 		};
+		const fetchDiagnoses = async () => {
+			try {
+				const diagnosesData = await diagnosesService.getAllDiagnoses();
+				setDiagnoses(diagnosesData);
+			} catch (error) {
+				console.error('Error fetching diagnoses:', error);
+			}
+		};
 
 		void fetchPatientData();
+		void fetchDiagnoses();
 	}, [id]);
+
 
 	const getGenderIcon = (gender: Gender) => {
 		if (gender === 'male') {
@@ -37,7 +50,18 @@ const PatientDetailPage = () => {
 
 	};
 
+	const getDiagnosisDescription = (code: string) => {
+		if (diagnoses) {
+			const diagnosis = diagnoses.find((d) => d.code === code);
+			return diagnosis ? diagnosis.name : "Unknown";
+		}
+		return "Loading...";
+	};
+
+
+	console.log(diagnoses);
 	console.log(patient);
+
 
 	return (
 		<div>
@@ -47,19 +71,20 @@ const PatientDetailPage = () => {
 					<Typography>ssn: {patient.ssn}</Typography>
 					<Typography>occupation: {patient.occupation}</Typography>
 					<Typography variant="h5">Entries:</Typography>
-					<Typography>{patient.entries[0].date} <i>{patient.entries[0].description}</i></Typography>
-					{patient.entries[0].diagnosisCodes ? (
-						<>
-							{/* <Typography variant="h6">Diagnosis Codes:</Typography> */}
-							<List dense disablePadding>
-								{patient.entries[0].diagnosisCodes.map((code, index) => (
-									<ListItem key={index}>
-										<ListItemText primary={code} />
-									</ListItem>
-								))}
-							</List>
-						</>
-					) : null}
+					{patient.entries.map((entry, index) => (
+						<div key={index}>
+							<Typography>{entry.date} <i>{entry.description}</i></Typography>
+							{entry.diagnosisCodes ? (
+								<List dense disablePadding>
+									{entry.diagnosisCodes.map((code, codeIndex) => (
+										<ListItem key={codeIndex}>
+											<ListItemText primary={code} secondary={getDiagnosisDescription(code)} />
+										</ListItem>
+									))}
+								</List>
+							) : null}
+						</div>
+					))}
 				</Box>
 			) : (
 				<div>Loading...</div>
